8af55fb8d9245e6731b86fd8142db04b7a406731
bmurena <bmurena@sudamericano.edu.ec>
2026-07-09 17:28:24 -0500

    Configurar CORS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PATCH,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
